97ec36581bba46bb035f1aa6a878c7c2fe8a85c1
Ritik varun <132301755+ritikvarun@users.noreply.github.com>
2026-04-19 17:07:03 +0530

    Update CORS configuration for development and production

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(
     origin:
       process.env.NODE_ENV === "production"
         ? [process.env.FRONTEND_URL]
-        : ["http://localhost:5173", "http://localhost:5174"],
+        : ["http://localhost:5173", "https://shop-x-teal.vercel.app/"],
     credentials: true,
   }),
 );
